6cf6263da37043c4c3c9f80417edfaeb7042a72d
Nivas <sainivaspvs@gmail.com>
2022-02-06 09:51:47 +0530

    added completion

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -51,6 +51,13 @@ return packer.startup(function(use)
   use 'Mofiqul/dracula.nvim'
   use 'EdenEast/nightfox.nvim'
 
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
