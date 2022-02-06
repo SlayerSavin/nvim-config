ff001b5356a129ba0d086352ab9a091b63d12a0a
Nivas <sainivaspvs@gmail.com>
2022-02-06 11:25:24 +0530

    added whichkey

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -111,6 +111,9 @@ return packer.startup(function(use)
   use "goolord/alpha-nvim"
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
 
+  -- Which key
+  use "folke/which-key.nvim"
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
